8469f84fc5a8cac9fa6c33dc60c89f998e3d8c52
wilkie <wilkie@xomb.org>
2015-07-15 03:44:38 -0400

    Adds several implementations. Working kinda.

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
-blah: ymf262.c test.c a2m.c
-	gcc -o blah ymf262.c test.c a2m.c -std=c99 -lm -lSDL
+blah: ymf262.c test.c a2m.c fmopl.c
+	#gcc -o blah ymf262.c test.c -std=c99 -lm -lSDL -g
+	g++ -o blah test.c dbopl.cpp -lm -lSDL -g
+	#g++ -o blah fmopl.c test.c -lm -lSDL -g -DHAS_YM3812
--- test.c
+++ test.c
@@ -1,66 +1,127 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
 
+#define USE_DOSBOX_OPL
+
+#ifdef USE_DOSBOX_OPL
+#include "dbopl.hpp"
+DBOPL::Handler dbopl_device;
+#elif defined(USE_YMF262_OPL)
+#define OPL3_INTERNAL_FREQ 14318180
 #include "ymf262.h"
+int ymf262_device_id = 0;
+#else
+#include "fmopl.h"
+FM_OPL* fmopl_device;
+#endif
+
+void playPair(FILE* f);
+
+unsigned int del = 0;
+unsigned int speed = 0;
+
+typedef struct {
+  FILE* f;
+} PlayDetails;
+
+PlayDetails current_song;
 
 SDL_AudioSpec sdl_audio_spec = {0};
 int sdl_sample_rate = 48000;
 int sdl_samples = 4096;
 int sample_frame_size = 0;
-int ymf262_device_id = 0;
 
 #define OPL3_SAMPLE_BITS   16
-#define OPL3_INTERNAL_FREQ 14400000
 
-char* ymf262_sample_buffer;
-char* ymf262_sample_buffer_channel[18] = {
+short* ymf262_sample_buffer;
+short* ymf262_sample_buffer_channel[18] = {
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
 
-void oml3_out(short reg, short data) {
+void opl3_out(short reg, short data) {
+#ifdef USE_DOSBOX_OPL
+  dbopl_device.WriteReg(reg, data);
+#else
   int op = 0;
   if (reg > 255) {
     op = 2;
     reg &= 0xff;
   }
-
+#ifdef USE_YMF262_OPL
   YMF262Write(ymf262_device_id, op,   reg);
   YMF262Write(ymf262_device_id, op+1, data);
+#else
+  OPLWrite(fmopl_device, op,   reg);
+  OPLWrite(fmopl_device, op+1, data);
+#endif
+#endif
 }
 
-void oml3_exp(short data) {
-  YMF262Write(ymf262_device_id, 2, data & 0xff);
-  YMF262Write(ymf262_device_id, 3, data >> 8);
-}
+float current_time = 0.0f;
 
 void callback(
     void*  userdata,
     Uint8* stream,
     int    length) {
-  char* buffer_table[18];
 
-  // For each sample:
-  for (int counter = 0; counter < (length / 4); counter++) {
-    // Update partial channel samples
-    for (int i = 0; i < 18; i++) {
-      buffer_table[i] = ymf262_sample_buffer_channel[i] + (counter * 4);
+#ifdef USE_DOSBOX_OPL
+  DBOPL::OPLMixer oplmixer((int16_t*)stream);
+#endif
+
+  short* buffer_table[18];
+
+  static long minicnt = 0;
+
+  long towrite = length / 4;
+
+  short* stream_pos = (short*)stream;
+
+  while(towrite > 0) {
+    while(minicnt < 0) {
+      minicnt += sdl_sample_rate;
+      if (current_song.f != NULL) {
+        playPair(current_song.f);
+      }
     }
 
-    // Update one step
-    YMF262UpdateOne(ymf262_device_id, (short*)(ymf262_sample_buffer + (counter * 4)), (short**)&buffer_table, 1);
-  }
+    float refresh = 1193180.0 / (speed ? speed : 0xffff);
 
-  // Copy buffer
-  memcpy(ymf262_sample_buffer, stream, length);
+    long i = (long)(minicnt / refresh + 4) & ~3;
+    if (towrite < i) {
+      i = towrite;
+    }
+
+#ifdef USE_DOSBOX_OPL
+    dbopl_device.Generate(&oplmixer, i);
+#elif defined(USE_YMF262_OPL)
+    YMF262UpdateOne(ymf262_device_id, stream_pos, NULL, i);
+#else
+    YM3812UpdateOne(fmopl_device, stream_pos, i);
+    // Duplicate channels
+    for (long p = i; p>0; p--) {
+      stream_pos[p*2-1] = stream_pos[p-1];
+      stream_pos[p*2-2] = stream_pos[p-1];
+    }
+#endif
+    stream_pos += i * 2;
+    towrite -= i;
+
+    minicnt -= (long)(refresh * i);
+  }
 }
 
 int uninit() {
-  // Uninit YMF Module
-  YMF262Shutdown();
-
   // Uninit SDL
   SDL_PauseAudio(1);
   SDL_CloseAudio();
+
+  // Uninit YMF Module
+#ifdef USE_DOSBOX_OPL
+#elif defined(USE_YMF262_OPL)
+  YMF262Shutdown();
+#else
+  OPLDestroy(fmopl_device);
+#endif
 
   // Deallocate
   if (ymf262_sample_buffer != NULL) {
@@ -74,12 +135,18 @@ int uninit() {
       ymf262_sample_buffer_channel[i] = NULL;
     }
   }
+
+  if (current_song.f != NULL) {
+    fclose(current_song.f);
+  }
 }
 
 int init() {
+  // Create current playing song structure
+  current_song.f = NULL;
+
   // Create buffers
-  sample_frame_size = sdl_sample_rate / 50;
-  ymf262_sample_buffer = malloc(sdl_samples * 4);
+  ymf262_sample_buffer = (short*)malloc(sdl_samples * 4);
 
   if (ymf262_sample_buffer == NULL) {
     // Out of memory
@@ -88,7 +155,7 @@ int init() {
   }
 
   for (int i = 0; i < 18; i++) {
-    ymf262_sample_buffer_channel[i] = malloc(sdl_samples * 4);
+    ymf262_sample_buffer_channel[i] = (short*)malloc(sdl_samples * 4);
     if (ymf262_sample_buffer_channel[i] == NULL) {
       // Out of memory
       uninit();
@@ -97,8 +164,21 @@ int init() {
   }
 
   // Initialize the YMF Module
+#ifdef USE_DOSBOX_OPL
+  dbopl_device.Init(sdl_sample_rate);
+#elif defined(USE_YMF262_OPL)
   ymf262_device_id = YMF262Init(1, OPL3_INTERNAL_FREQ, sdl_sample_rate);
   YMF262ResetChip(ymf262_device_id);
+#else
+  fmopl_device = OPLCreate(OPL_TYPE_YM3812, 3579545, sdl_sample_rate);
+  OPLResetChip(fmopl_device);
+#endif
+
+  // 4-waveform Select (paranoia)
+  opl3_out(1, 32);
+
+  // Enable OPL3
+  //opl3_out(105, 1);
 
   // Initialize SDL Audio
   sdl_audio_spec.freq = sdl_sample_rate;
@@ -117,19 +197,100 @@ int init() {
     return -1;
   }
 
-  SDL_PauseAudio(0);
+  printf("opened device, sample rate: %d, samples: %d\n", have.freq, have.samples);
+  sdl_sample_rate = have.freq;
+  sdl_samples = have.samples;
 
   return 0;
+}
+
+void setchip(const char num) {
+  printf("chip??\n");
+}
+
+void playPair(FILE* f) {
+  unsigned int setspeed = 0;
+
+  unsigned char param = 0;
+  unsigned char command = 0;
+
+  if(del) {
+    del--;
+    return;
+  }
+
+  do {
+    setspeed = 0;
+
+    fread(&param, 1, 1, f);
+    fread(&command, 1, 1, f);
+
+    switch(command) {
+    case 0:
+      del = param - 1;
+      break;
+    case 2:
+      if(!param) {
+        speed = param + (command << 8);
+        setspeed = 1;
+      } else
+        setchip(param - 1);
+      break;
+    case 0xff:
+      if(param == 0xff) {
+        printf("END OF SONG\n");
+        return; // End of song
+      }
+      break;
+    default:
+      opl3_out(command, param);
+      break;
+    }
+  } while((command || setspeed) && !feof(f));
+
+  return;
+}
+
+void playRAW(const char* filename) {
+  FILE* f = fopen(filename, "r");
+
+  if (f == NULL) {
+    return;
+  }
+
+  char signature[9] = {0};
+
+  fread(&signature, 8, 1, f);
+  if(strncmp(signature,"RAWADATA",8)) {
+    fclose(f);
+    printf("Error loading RAW.\n");
+    return;
+  }
+
+  // load section
+  unsigned short clock;
+  fread(&clock, 2, 1, f);
+  speed = clock;
+
+  current_song.f = f;
 }
 
 int main(int argc, char** argv) {
   int initialized = init();
   if (initialized != 0) {
-    printf("error\n");
+    printf("Error.\n");
     return -1;
   }
 
-  SDL_Delay(5000);
+  if (argc > 1) {
+    playRAW(argv[1]);
+  }
+
+  // Play
+  SDL_PauseAudio(0);
+
+  // Wait until it finishes
+  SDL_Delay(1000000);
 
   uninit();
 

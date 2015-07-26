0474b416180b3c575b2ee3521fda8b11cc672b5d
wilkie <wilkie@xomb.org>
2015-07-26 03:18:19 -0400

    Uses proper place to find extension library.

--- lib/beepboop.rb
+++ lib/beepboop.rb
@@ -1,7 +1,13 @@
 $:.unshift File.dirname(__FILE__) + "/../ext"
 $:.unshift File.dirname(__FILE__)
 
-require_relative '../beepboop'
+module Beepboop
+end
+
+# C extensions
+require 'beepboop/beepboop'
+
+# Rest of the stuff
 require 'beepboop/audio_device'
 require 'beepboop/opl'
 require 'beepboop/player'
